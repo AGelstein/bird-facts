85295dc0f8e680daab3beb0d3dda2b11705caea3
AGelstein <gelstein.andrew@gmail.com>
2024-10-13 15:43:19 -0500

    building up entity for create and setting up for db connection

--- backend/src/bird/bird.controller.ts
+++ backend/src/bird/bird.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Post, Get, Body } from '@nestjs/common';
 import { BirdService } from './bird.service';
 import { Bird } from './entities/bird.entity';
 import { CreateBirdDto } from './dto/create-bird.dto';
+import { v4 as uuidv4 } from 'uuid';
 
 @Controller('bird')
 export class BirdController {
@@ -9,7 +10,16 @@ export class BirdController {
   
   @Post()
   async create(@Body() createBirdDto: CreateBirdDto) {
-    this.birdService.create(createBirdDto);
+
+    const bird: Bird = {
+      id: uuidv4(),
+      name: createBirdDto.name,
+      color: createBirdDto.color,
+      breed: createBirdDto.breed,
+      age: createBirdDto.age,
+    };
+    
+    this.birdService.create(bird);
   }
 
   @Get()
--- backend/src/bird/entities/bird.entity.ts
+++ backend/src/bird/entities/bird.entity.ts
@@ -1,1 +1,15 @@
-export class Bird {}
+export class Bird {
+    id: string;
+    name: string;
+    color: string;
+    breed: string;
+    age: number;
+
+    constructor(id: string, name: string, color: string, breed: string, age: number) {
+        this.id = id;
+        this.name = name;
+        this.color = color;
+        this.breed = breed;
+        this.age = age;
+    }
+}
