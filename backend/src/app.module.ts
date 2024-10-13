0aeb2d7b37da79d2acbdf43762358749112e64f5
AGelstein <gelstein.andrew@gmail.com>
2024-10-13 17:25:57 -0500

    adding these other modules to follow along with the example format. got the database module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BirdModule } from './bird/bird.module';
 import { BirdController } from './bird/bird.controller';
+import { UsersModule } from './users/users.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [BirdModule],
+  imports: [BirdModule, UsersModule, DatabaseModule],
   controllers: [AppController, BirdController],
   providers: [AppService],
 })
