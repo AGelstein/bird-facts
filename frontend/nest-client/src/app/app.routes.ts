37da41d77d1b514273a098fc6e09c342e13a27ca
AGelstein <gelstein.andrew@gmail.com>
2024-11-07 17:33:53 -0600

    Clean up routes. Install primeng. remove eslint conflicts

--- frontend/nest-client/src/app/app.routes.ts
+++ frontend/nest-client/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { UserCardComponent } from './components/user-card/user-card.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: UserCardComponent,
+        pathMatch: 'full',
+      },
+];
