a0f86914150a48944e280db74f030dafa30a26b6
AGelstein <gelstein.andrew@gmail.com>
2024-11-09 09:51:22 -0600

    finish primeng import and now can verify with icons and elements

--- frontend/nest-client/src/app/components/user-card/user-card.component.ts
+++ frontend/nest-client/src/app/components/user-card/user-card.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-
+import { ButtonModule } from 'primeng/button';
 @Component({
   selector: 'app-user-card',
   standalone: true,
-  imports: [],
+  imports: [ButtonModule],
   templateUrl: './user-card.component.html',
   styleUrl: './user-card.component.css'
 })
